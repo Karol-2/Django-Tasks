2bebdc6148e073920e0921e7bf910d3a908b0e73
Karol Krawczykiewicz <26karol26@gmail.com>
2024-03-07 18:01:37 +0100

    Add Task and Category models

--- TaskApp/models.py
+++ TaskApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.CharField(max_length=255)
+    completed = models.BooleanField()
+    category = models.ForeignKey(Category, related_name='tasks', on_delete=models.CASCADE)
+
+
