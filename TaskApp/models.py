43b8b660c4c780c4c94cf2ff9789dbb0fa30bfc1
Karol Krawczykiewicz <26karol26@gmail.com>
2024-03-07 18:18:07 +0100

    Add admin panel

--- TaskApp/models.py
+++ TaskApp/models.py
@@ -5,12 +5,18 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=25)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     title = models.CharField(max_length=25)
     description = models.CharField(max_length=255)
     completed = models.BooleanField()
     category = models.ForeignKey(Category, related_name='tasks', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 # python manage.py makemigrations
