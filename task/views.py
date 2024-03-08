93daa491291148d251c4d9473183ebd92fcab694
Karol Krawczykiewicz <26karol26@gmail.com>
2024-03-08 09:19:24 +0100

    Add API authentication

--- task/views.py
+++ task/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from .models import Task, Category
 from .serializers import TaskSerializer, CategorySerializer
@@ -7,19 +8,23 @@ from .serializers import TaskSerializer, CategorySerializer
 class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class CategoryList(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [IsAuthenticated]
 
