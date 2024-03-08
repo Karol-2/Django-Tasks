8d0f9c2441a580f487102ea58df4da1603af6680
Karol Krawczykiewicz <26karol26@gmail.com>
2024-03-08 09:09:29 +0100

    Fix api urls

--- task/urls.py
+++ task/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import TaskList, TaskDetail, CategoryList, CategoryDetail
 
 urlpatterns =  [
-    path('task/', TaskList.as_view()),
-    path('task/<int:pk>/', TaskDetail.as_view()),
-    path('category/', CategoryList.as_view()),
-    path('category/<int:pk>', CategoryDetail.as_view()),
+    path('tasks', TaskList.as_view()),
+    path('tasks/<int:pk>', TaskDetail.as_view()),
+    path('categories', CategoryList.as_view()),
+    path('categories/<int:pk>', CategoryDetail.as_view()),
 ]
